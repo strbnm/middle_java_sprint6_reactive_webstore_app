93e625f20df66346d389fbd41a3dbb2864a9fbb1
Pavel <strbnm@yandex.ru>
2025-02-17 19:27:36 +0300

    Добавлен основной класс приложения

---
+++ src/main/java/ru/strbnm/store/WebStoreApplication.java
@@ -0,0 +1,12 @@
+package ru.strbnm.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStoreApplication.class, args);
+    }
+}
