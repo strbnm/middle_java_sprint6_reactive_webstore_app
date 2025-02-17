f28335a039aadb5693b7508dfab2dcfc689d3398
Pavel <strbnm@yandex.ru>
2025-02-17 21:11:17 +0300

    Добавлен интерфейс ProductService

---
+++ src/main/java/ru/strbnm/store/service/ProductService.java
@@ -0,0 +1,19 @@
+package ru.strbnm.store.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import ru.strbnm.store.dto.ProductDTO;
+import ru.strbnm.store.entity.Product;
+
+import java.math.BigDecimal;
+
+public interface ProductService {
+    Page<ProductDTO> findAllProductPagingAndSorting(Pageable pageable);
+    Page<ProductDTO> findAllProductByNameOrDescriptionContaining(String search, Pageable pageable);
+    Page<ProductDTO> findAllProductByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable);
+    Page<ProductDTO> findAllProductByNameStartsWith(String search, Pageable pageable);
+    ProductDTO getProductById (Long id);
+    Product getOne(Long id);
+    ProductDTO saveProduct(Product product);
+    void deleteProduct(Long id);
+}
