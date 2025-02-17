118b4befc5cc809e5c47e30bdbea02ccc6235a8a
Pavel <strbnm@yandex.ru>
2025-02-17 21:11:50 +0300

    Добавлена реализация интерфейса ProductServiceImpl

--- src/main/java/ru/strbnm/store/repository/ProductRepository.java
+++ src/main/java/ru/strbnm/store/repository/ProductRepository.java
@@ -1,6 +1,16 @@
 package ru.strbnm.store.repository;
 
+import java.math.BigDecimal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.strbnm.store.entity.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+  Page<Product> findProductsByNameOrDescriptionContainingIgnoreCase(
+      String name, String description, Pageable pageable);
+
+  Page<Product> findProductByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable);
+
+  Page<Product> findProductsByNameStartsWithIgnoreCase(String letter, Pageable pageable);
+}
---
+++ src/main/java/ru/strbnm/store/service/ProductServiceImpl.java
@@ -0,0 +1,67 @@
+package ru.strbnm.store.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import ru.strbnm.store.dto.ProductDTO;
+import ru.strbnm.store.entity.Product;
+import ru.strbnm.store.mapper.ProductMapper;
+import ru.strbnm.store.repository.ProductRepository;
+
+import java.math.BigDecimal;
+
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
+    @Override
+    public Page<ProductDTO> findAllProductPagingAndSorting(Pageable pageable) {
+        return productRepository.findAll(pageable).map(productMapper::toDTO);
+    }
+
+    @Override
+    public Page<ProductDTO> findAllProductByNameOrDescriptionContaining(String search, Pageable pageable) {
+        return productRepository.findProductsByNameOrDescriptionContainingIgnoreCase(search, search, pageable).map(productMapper::toDTO);
+    }
+
+    @Override
+    public Page<ProductDTO> findAllProductByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable) {
+        return productRepository.findProductByPriceBetween(minPrice, maxPrice, pageable). map(productMapper::toDTO);
+    }
+
+    @Override
+    public Page<ProductDTO> findAllProductByNameStartsWith(String letter, Pageable pageable) {
+        return productRepository.findProductsByNameStartsWithIgnoreCase(letter, pageable).map(productMapper::toDTO);
+    }
+
+    @Override
+    public ProductDTO getProductById(Long id) {
+        Product existsProduct = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Товар не найден"));
+        return productMapper.toDTO(existsProduct);
+    }
+
+    @Override
+    public Product getOne(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Товар не найден"));
+    }
+
+    @Override
+    public ProductDTO saveProduct(Product product) {
+        return productMapper.toDTO(productRepository.save(product));
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
