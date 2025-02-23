703a1e3208360b98a43b8827292d54e444b6bb64
Pavel <strbnm@yandex.ru>
2025-02-24 00:33:31 +0300

    Добавление методов работы с корзиной

--- src/main/java/ru/strbnm/store/controller/CartController.java
+++ src/main/java/ru/strbnm/store/controller/CartController.java
@@ -1,18 +1,28 @@
 package ru.strbnm.store.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.strbnm.store.dto.CartDto;
 import ru.strbnm.store.dto.CartItemDto;
+import ru.strbnm.store.entity.CartItem;
 import ru.strbnm.store.service.CartService;
 
-@RestController
+@Controller
 @RequestMapping("/cart")
-@RequiredArgsConstructor
 public class CartController {
   private final CartService cartService;
 
+  @Autowired
+  public CartController(CartService cartService) {
+    this.cartService = cartService;
+  }
+
   @PostMapping
+  @ResponseBody
   public ResponseEntity<CartItemDto> addToCart(@RequestBody CartItemDto cartItemDto) {
     CartItemDto savedItem =
         cartService.addToCart(cartItemDto.getProductId(), cartItemDto.getQuantity());
@@ -20,10 +30,28 @@ public class CartController {
   }
 
   @PutMapping
+  @ResponseBody
   public ResponseEntity<CartItemDto> updateCartItem(@RequestBody CartItemDto cartItemDto) {
     CartItemDto updatedItem =
         cartService.updateCartItem(
             cartItemDto.getId(), cartItemDto.getProductId(), cartItemDto.getQuantity());
     return ResponseEntity.ok(updatedItem);
+  }
+
+  @DeleteMapping("/{cartItemId}")
+  @ResponseBody
+  public String removeCartItem(@PathVariable(value = "cartItemId") Long cartItemId) {
+    cartService.removeFromCart(cartItemId);
+    return "success";
+  }
+
+  @GetMapping
+  public String getCart(Model m) {
+    CartDto cart = cartService.getCartSummary();
+    // Вычисляем сумму количества товаров в корзине
+    int cartTotalQuantity = cart.getCartItems().stream().mapToInt(CartItem::getQuantity).sum();
+    m.addAttribute("cart", cart);
+    m.addAttribute("totalQuantity", cartTotalQuantity);
+    return "cart/showcart";
   }
 }
