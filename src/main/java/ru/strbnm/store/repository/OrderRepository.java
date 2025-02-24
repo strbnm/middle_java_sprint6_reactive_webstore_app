06d30dd02938bc5d5b20ee42ca8665ef7cc3019e
Pavel <strbnm@yandex.ru>
2025-02-25 00:48:48 +0300

    Добавление метода для получения всех связанных объектов

--- src/main/java/ru/strbnm/store/repository/OrderRepository.java
+++ src/main/java/ru/strbnm/store/repository/OrderRepository.java
@@ -1,6 +1,13 @@
 package ru.strbnm.store.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.strbnm.store.entity.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT o FROM Order o LEFT JOIN FETCH o.items i LEFT JOIN FETCH i.product WHERE o.id = :orderId")
+    Optional<Order> findOrderWithItemsAndProducts(@Param("orderId") Long orderId);
+}
