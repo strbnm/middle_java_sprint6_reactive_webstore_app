1a0794e6406ba9537c4782be40fc5f3a33587707
Pavel <strbnm@yandex.ru>
2025-03-02 00:17:13 +0300

    Добавлены тесты для CartControllerIntegrationTest

--- src/test/java/ru/strbnm/store/controller/CartControllerIntegrationTest.java
+++ src/test/java/ru/strbnm/store/controller/CartControllerIntegrationTest.java
@@ -3,8 +3,8 @@ package ru.strbnm.store.controller;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.hasItem;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -30,7 +30,7 @@ public class CartControllerIntegrationTest extends BaseControllersIntegrationTes
 
   @Autowired private ObjectMapper objectMapper;
 
-  private CartItemDto cartItemDtoA, cartItemDtoB;
+  private CartItemDto cartItemDtoA, cartItemDtoB, cartItemDtoC;
 
   /*
    После выполнения скрипта INIT_STORE_RECORD.sql таблица cart-items содержит следующие записи:
@@ -46,6 +46,7 @@ public class CartControllerIntegrationTest extends BaseControllersIntegrationTes
   void setUp() {
     cartItemDtoA = CartItemDto.builder().productId(1L).quantity(5).build();
     cartItemDtoB = CartItemDto.builder().id(1L).productId(2L).quantity(11).build();
+    cartItemDtoC = CartItemDto.builder().id(1L).productId(2L).quantity(0).build();
   }
 
   @Transactional
@@ -67,7 +68,6 @@ public class CartControllerIntegrationTest extends BaseControllersIntegrationTes
         .andExpect(jsonPath("$.cartItem.productId").value(cartItemDtoA.getProductId()))
         .andExpect(jsonPath("$.cartItem.quantity").value(cartItemDtoA.getQuantity()))
         .andExpect(jsonPath("$.cartItemsCount").value(66));
-    ;
 
     List<CartItem> cartItemsAfterAdd = cartItemRepository.findAll();
     assertThat("Позиций в корзине после добавления должно быть 5.", cartItemsAfterAdd, hasSize(5));
@@ -80,7 +80,7 @@ public class CartControllerIntegrationTest extends BaseControllersIntegrationTes
 
   @Transactional
   @Test
-  void UpdateCartItem_shouldUpdateCartItemInDatabaseAndReturnCartItemInfoDto() throws Exception {
+  void updateCartItem_shouldUpdateCartItemInDatabaseAndReturnCartItemInfoDto() throws Exception {
     List<CartItem> cartItemsBeforeUpdate = cartItemRepository.findAll();
     assertThat("Позиций в корзине до обновления должно быть 4.", cartItemsBeforeUpdate, hasSize(4));
     List<String> oldProductNames =
@@ -97,12 +97,107 @@ public class CartControllerIntegrationTest extends BaseControllersIntegrationTes
         .andExpect(jsonPath("$.cartItem.productId").value(cartItemDtoB.getProductId()))
         .andExpect(jsonPath("$.cartItem.quantity").value(cartItemDtoB.getQuantity()))
         .andExpect(jsonPath("$.cartItemsCount").value(62));
-    ;
 
-    List<CartItem> cartItemsAfterAdd = cartItemRepository.findAll();
-    assertThat("Позиций в корзине после обновления должно быть 4.", cartItemsAfterAdd, hasSize(4));
+    List<CartItem> cartItemsAfterUpdate = cartItemRepository.findAll();
+    assertThat(
+        "Позиций в корзине после обновления должно быть 4.", cartItemsAfterUpdate, hasSize(4));
     List<String> newProductNames =
-        cartItemsAfterAdd.stream().map(cartItem -> cartItem.getProduct().getName()).toList();
+        cartItemsAfterUpdate.stream().map(cartItem -> cartItem.getProduct().getName()).toList();
     assertThat(newProductNames, hasItems(oldProductNames.toArray(new String[0])));
+  }
+
+  @Test
+  void updateCartItemWithZeroQuantity_shouldDeleteCartItemInDatabaseAndReturnCartItemInfoDto()
+      throws Exception {
+    List<CartItem> cartItemsBeforeUpdate = cartItemRepository.findAll();
+    assertThat("Позиций в корзине до обновления должно быть 4.", cartItemsBeforeUpdate, hasSize(4));
+
+    mockMvc
+        .perform(
+            put("/cart")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(cartItemDtoC)))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.cartItem.id").value(cartItemDtoC.getId()))
+        .andExpect(jsonPath("$.cartItem.productId").value(cartItemDtoC.getProductId()))
+        .andExpect(jsonPath("$.cartItem.quantity").value(cartItemDtoC.getQuantity()))
+        .andExpect(jsonPath("$.cartItemsCount").value(51));
+
+    List<CartItem> cartItemsAfterUpdate = cartItemRepository.findAll();
+    assertThat(
+        "Позиций в корзине после обновления должно быть 3.", cartItemsAfterUpdate, hasSize(3));
+    CartItem deletedCartItem =
+        cartItemsAfterUpdate.stream()
+            .filter(cartItem -> cartItem.getId().equals(cartItemDtoC.getId()))
+            .findFirst()
+            .orElse(null);
+    assertNull(deletedCartItem, "Объект должен быть null.");
+  }
+
+  @Test
+  void removeCartItem_shouldDeleteCartItemInDatabaseAndReturnCartItemInfoDto() throws Exception {
+    List<CartItem> cartItemsBeforeUpdate = cartItemRepository.findAll();
+    assertThat("Позиций в корзине до обновления должно быть 4.", cartItemsBeforeUpdate, hasSize(4));
+
+    mockMvc
+        .perform(delete("/cart/" + cartItemDtoB.getId()).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.cartItemsCount").value(51));
+
+    List<CartItem> cartItemsAfterUpdate = cartItemRepository.findAll();
+    assertThat(
+        "Позиций в корзине после обновления должно быть 3.", cartItemsAfterUpdate, hasSize(3));
+    CartItem deletedCartItem =
+        cartItemsAfterUpdate.stream()
+            .filter(cartItem -> cartItem.getId().equals(cartItemDtoB.getId()))
+            .findFirst()
+            .orElse(null);
+    assertNull(deletedCartItem, "Объект должен быть null.");
+  }
+
+  @Test
+  void getCart_shouldReturnHtmlWithCart() throws Exception {
+    mockMvc
+        .perform(get("/cart"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("text/html;charset=UTF-8"))
+        .andExpect(view().name("cart/index"))
+        .andExpect(model().attributeExists("cart", "cartTotalQuantity"))
+        // Проверка, что Заголовок страницы "Все публикации"
+        .andExpect(xpath("//main/h4").string("Товары в корзине"))
+        // Проверяем, что 4 элемента в корзине
+        .andExpect(xpath("//ul[@class='collection']/li").nodeCount(4))
+        // Проверяем название, цену и количество для первого элемента корзины
+        .andExpect(
+            xpath("//ul[@class='collection']/li[1]/span[@class='title']")
+                .string("Ultra Gadget 123"))
+        .andExpect(xpath("//ul[@class='collection']/li[1]/p").string("Цена: 191.38 руб."))
+        .andExpect(
+            xpath("//ul[@class='collection']/li[1]//input[@class='quantity-input']/@value")
+                .string("10"))
+        // Проверяем название, цену и количество для второго элемента корзины
+        .andExpect(
+            xpath("//ul[@class='collection']/li[2]/span[@class='title']")
+                .string("Ultra Widget 734"))
+        .andExpect(xpath("//ul[@class='collection']/li[2]/p").string("Цена: 84.71 руб."))
+        .andExpect(
+            xpath("//ul[@class='collection']/li[2]//input[@class='quantity-input']/@value")
+                .string("20"))
+        // Проверяем название, цену и количество для третьего элемента корзины
+        .andExpect(
+            xpath("//ul[@class='collection']/li[3]/span[@class='title']").string("Ultra Item 402"))
+        .andExpect(xpath("//ul[@class='collection']/li[3]/p").string("Цена: 289.61 руб."))
+        .andExpect(
+            xpath("//ul[@class='collection']/li[3]//input[@class='quantity-input']/@value")
+                .string("30"))
+        // Проверяем название, цену и количество для четвертого элемента корзины
+        .andExpect(
+            xpath("//ul[@class='collection']/li[4]/span[@class='title']").string("Eco Thing 576"))
+        .andExpect(xpath("//ul[@class='collection']/li[4]/p").string("Цена: 211.65 руб."))
+        .andExpect(
+            xpath("//ul[@class='collection']/li[4]//input[@class='quantity-input']/@value")
+                .string("1"));
   }
 }
