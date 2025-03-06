77f4385885fc32797bb58a1c0c2c7f2576b2419b
Pavel <strbnm@yandex.ru>
2025-03-06 20:20:39 +0300

    Обновление файла конфигурации в части директорий статики и изображений продуктов

--- src/main/java/ru/strbnm/store/config/WebConfig.java
+++ src/main/java/ru/strbnm/store/config/WebConfig.java
@@ -1,22 +1,43 @@
 package ru.strbnm.store.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
   @Value("${app.product.image.dir}")
   private String productImageDir;
 
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry
-        .addResourceHandler("/product_image_dir/**")
-        .addResourceLocations("file:" + productImageDir + "/");
-    registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
-    registry.addResourceHandler("/imgs/**").addResourceLocations("classpath:/static/imgs/");
+  @Bean
+  public RouterFunction<ServerResponse> staticResourceRouter() {
+    return route(GET("/product_image_dir/{filename}"), request -> {
+      String filename = request.pathVariable("filename");
+      Path imagePath = Paths.get(productImageDir, filename);
+      Resource imageResource = new PathResource(imagePath.toUri());
+
+      return ok()
+              .contentType(MediaType.IMAGE_PNG)
+              .bodyValue(imageResource);
+    })
+            .and(RouterFunctions.resources("/css/**", new ClassPathResource("static/css/")))
+            .and(RouterFunctions.resources("/imgs/**", new ClassPathResource("static/imgs/")));
   }
 }
+
+
