ac04ca9b8b3a9f5203a40802239069c92472920a
Pavel <strbnm@yandex.ru>
2025-03-08 16:32:01 +0300

    Рефакторинг OrderItemRepository для работы с R2DBC

--- src/main/java/ru/strbnm/store/repository/OrderItemRepository.java
+++ src/main/java/ru/strbnm/store/repository/OrderItemRepository.java
@@ -1,6 +1,8 @@
 package ru.strbnm.store.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.strbnm.store.entity.OrderItem;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {}
+@Repository
+public interface OrderItemRepository extends ReactiveCrudRepository<OrderItem, Long> {}
