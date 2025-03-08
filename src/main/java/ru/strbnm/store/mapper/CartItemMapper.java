ac3c376b722e4d3368a4ca105c2f0b4540c28080
Pavel <strbnm@yandex.ru>
2025-03-08 16:33:23 +0300

    Рефакторинг CartItemMapper

--- src/main/java/ru/strbnm/store/mapper/CartItemMapper.java
+++ src/main/java/ru/strbnm/store/mapper/CartItemMapper.java
@@ -2,18 +2,14 @@ package ru.strbnm.store.mapper;
 
 import java.util.List;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.strbnm.store.dto.CartItemDto;
 import ru.strbnm.store.entity.CartItem;
 
 @Mapper(componentModel = "spring")
 public interface CartItemMapper {
-  @Mapping(source = "product.id", target = "productId")
   CartItemDto toDto(CartItem cartItem);
 
-  @Mapping(source = "productId", target = "product.id")
   CartItem toEntity(CartItemDto cartItemDto);
 
-  @Mapping(source = "productId", target = "product.id")
   List<CartItemDto> toDTOs(List<CartItem> cartItems);
 }
