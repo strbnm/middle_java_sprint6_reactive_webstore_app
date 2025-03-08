0e064fb16623c6a8ad99492a9039b67876a65c7d
Pavel <strbnm@yandex.ru>
2025-03-08 16:50:47 +0300

    Рефакторинг класса ProductRepositoryTest для тестов R2DBC

--- src/test/java/ru/strbnm/store/repository/ProductRepositoryTest.java
+++ src/test/java/ru/strbnm/store/repository/ProductRepositoryTest.java
@@ -4,18 +4,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.math.BigDecimal;
-import java.util.List;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
-import ru.strbnm.store.entity.Product;
-import ru.strbnm.store.repository.spec.ProductSpecifications;
+import reactor.test.StepVerifier;
+import ru.strbnm.store.testcases.ProductTestCase;
+import ru.strbnm.store.utils.ProductSortEnum;
 
-class ProductRepositoryTest extends BaseRepositoryTest {
+@Slf4j
+class ProductRepositoryTest extends BaseR2dbcTest {
 
   @Autowired private ProductRepository productRepository;
 
@@ -35,70 +34,35 @@ class ProductRepositoryTest extends BaseRepositoryTest {
   10      'Eco Item 319'      'Description for product_10'            'product_10.png'        301.50
   */
 
-  @Test
-  void findById_shouldReturnProductById() {
-    Product foundProduct = productRepository.findById(1L).orElse(null);
-
-    assertNotNull(foundProduct, "Объект товара не должен быть null.");
-    assertEquals(foundProduct.getId(), 1L, "Id товара должен быть равен 1.");
-    assertEquals(
-        foundProduct.getName(),
-        "Eco Gadget 690",
-        "Название товара с id=1 должно быть 'Eco Gadget 690'.");
+  @ParameterizedTest
+  @MethodSource("provideTestCases")
+  void findFilteredProducts_shouldReturnAllFilteredProductsSortingAndPaging(ProductTestCase.TestCase testCase) {
+    productRepository
+        .findFilteredProducts(
+            testCase.text(),
+            testCase.price_from(),
+            testCase.price_to(),
+            testCase.letter(),
+            testCase.page(),
+            testCase.size(),
+            testCase.sorting() != null
+                ? ProductSortEnum.valueOf(testCase.sorting()).getSortExpression()
+                : ProductSortEnum.NAME_ASC.getSortExpression())
+        .collectList()
+        .as(StepVerifier::create)
+        .assertNext(
+            filteredProducts -> {
+              log.info("filteredProducts={}", filteredProducts);
+              assertThat(filteredProducts, hasSize(testCase.countOnPage()));
+              assertEquals(testCase.firstProductName(), filteredProducts.getFirst().getName());
+              assertEquals(testCase.firstProductPrice(), filteredProducts.getFirst().getPrice());
+              assertEquals(testCase.lastProductName(), filteredProducts.getLast().getName());
+              assertEquals(testCase.lastProductPrice(), filteredProducts.getLast().getPrice());
+            })
+        .verifyComplete();
   }
 
-  @Test
-  void FindAll_shouldReturnAllProductsByPageAndSortByIdDesc() {
-    Page<Product> products =
-        productRepository.findAll(PageRequest.of(0, 5, Sort.by("id").descending()));
-    List<Long> actualProductIds = products.getContent().stream().map(Product::getId).toList();
-
-    assertEquals(
-        10L, products.getTotalElements(), "Общее количество товаров должно быть равно 10.");
-    assertEquals(
-        2, products.getTotalPages(), "Общее количество страниц постов должно быть равно 2.");
-    assertEquals(
-        actualProductIds, List.of(10L, 9L, 8L, 7L, 6L), "В списке id должны быть 10, 9, 8, 7 и 6.");
-    assertEquals(
-        products.getContent().getFirst().getName(),
-        "Eco Item 319",
-        "Первым в списке должен быть товар с названием 'Eco Item 319Eco Item 319'.");
-  }
-
-  @Test
-  void deleteById_shouldDeleteProductFromDatabase() {
-    productRepository.deleteById(5L);
-    List<Product> products = productRepository.findAll();
-    List<Product> deletedProducts =
-        products.stream().filter(post -> post.getId().equals(5L)).toList();
-
-    assertThat("Должно быть 9 товаров.", products, hasSize(9));
-    assertTrue(
-        deletedProducts.isEmpty(), "Должен быть пустой список результатов для товаров с id=5.");
-  }
-
-  // Тесты ProductSpecifications
-  @Test
-  void testFindByNameOrDescriptionContains() {
-    Specification<Product> spec = ProductSpecifications.nameOrDescriptionContains("Gadget");
-    List<Product> results = productRepository.findAll(spec);
-    assertThat(
-        "Должно быть две записи с 'Gadget' в названии или описании товара", results, hasSize(2));
-  }
-
-  @Test
-  void testFindByPriceBetween() {
-    Specification<Product> spec =
-        ProductSpecifications.priceBetween(new BigDecimal("100"), new BigDecimal("500"));
-    List<Product> results = productRepository.findAll(spec);
-    assertThat("Должно быть 8 записей с ценой в диапазоне [100, 500].", results, hasSize(8));
-  }
-
-  @Test
-  void testFindByNameStartsWith() {
-    Specification<Product> spec = ProductSpecifications.nameStartsWith("Eco");
-    List<Product> results = productRepository.findAll(spec);
-    assertThat(
-        "Должно быть три записи, название которых начинается на 'Eco'.", results, hasSize(3));
+  private Stream<ProductTestCase.TestCase> provideTestCases() {
+    return ProductTestCase.provideTestCases();
   }
 }
