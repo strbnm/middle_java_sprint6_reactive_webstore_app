0f9467dfb9621d8f28814538fc903f6c98d1f02c
Pavel <strbnm@yandex.ru>
2025-03-08 16:36:23 +0300

    Рефакторинг контроллера CartController для поддержки WebFlux

--- src/main/java/ru/strbnm/store/controller/CartController.java
+++ src/main/java/ru/strbnm/store/controller/CartController.java
@@ -5,11 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.strbnm.store.dto.CartDto;
+import reactor.core.publisher.Mono;
 import ru.strbnm.store.dto.CartInfoDto;
 import ru.strbnm.store.dto.CartItemDto;
 import ru.strbnm.store.dto.CartItemInfoDto;
-import ru.strbnm.store.entity.CartItem;
 import ru.strbnm.store.service.CartService;
 
 @Controller
@@ -24,48 +23,49 @@ public class CartController {
 
   @PostMapping
   @ResponseBody
-  public ResponseEntity<CartItemInfoDto> addToCart(@RequestBody CartItemDto cartItemDto) {
-    CartItemDto savedItem =
-        cartService.addToCart(cartItemDto.getProductId(), cartItemDto.getQuantity());
-    CartItemInfoDto cartItemInfo = getCartItemInfoDto(savedItem);
-    return ResponseEntity.ok(cartItemInfo);
+  public Mono<ResponseEntity<CartItemInfoDto>> addToCart(@RequestBody CartItemDto cartItemDto) {
+    return cartService
+        .addToCart(cartItemDto.getProductId(), cartItemDto.getQuantity())
+        .flatMap(this::getCartItemInfoDto)
+        .map(ResponseEntity::ok);
   }
 
   @PutMapping
   @ResponseBody
-  public ResponseEntity<CartItemInfoDto> updateCartItem(@RequestBody CartItemDto cartItemDto) {
-    CartItemDto updatedItem =
-        cartService.updateCartItem(
-            cartItemDto.getId(), cartItemDto.getProductId(), cartItemDto.getQuantity());
-    CartItemInfoDto cartItemInfo = getCartItemInfoDto(updatedItem);
-    return ResponseEntity.ok(cartItemInfo);
+  public Mono<ResponseEntity<CartItemInfoDto>> updateCartItem(
+      @RequestBody CartItemDto cartItemDto) {
+    return cartService
+        .updateCartItem(cartItemDto.getId(), cartItemDto.getProductId(), cartItemDto.getQuantity())
+        .flatMap(this::getCartItemInfoDto)
+        .map(ResponseEntity::ok);
   }
 
   @DeleteMapping("/{cartItemId}")
   @ResponseBody
-  public ResponseEntity<CartInfoDto> removeCartItem(
-      @PathVariable(value = "cartItemId") Long cartItemId) {
-    cartService.removeFromCart(cartItemId);
-    CartInfoDto cartInfo = cartService.getCartInfo();
-    return ResponseEntity.ok(cartInfo);
+  public Mono<ResponseEntity<CartInfoDto>> removeCartItem(@PathVariable Long cartItemId) {
+    return cartService
+        .removeFromCart(cartItemId)
+        .then(cartService.getCartInfo())
+        .map(ResponseEntity::ok);
   }
 
   @GetMapping
-  public String getCart(Model m) {
-    CartDto cart = cartService.getCartSummary();
-    // Вычисляем сумму количества товаров в корзине
-    int cartTotalQuantity = cart.getCartItems().stream().mapToInt(CartItem::getQuantity).sum();
-    m.addAttribute("cart", cart);
-    m.addAttribute("cartTotalQuantity", cartTotalQuantity);
-    return "cart/index";
+  public Mono<String> getCart(Model m) {
+    return cartService
+        .getCartSummary()
+        .doOnNext(cart -> m.addAttribute("cart", cart))
+        .then(Mono.just("cart/index"));
   }
 
-  private CartItemInfoDto getCartItemInfoDto(CartItemDto cartItem) {
-    CartInfoDto cartInfo = cartService.getCartInfo();
-    return CartItemInfoDto.builder()
-        .cartItem(cartItem)
-        .cartItemsCount(cartInfo.getCartItemsCount())
-        .cartAmount(cartInfo.getCartAmount())
-        .build();
+  private Mono<CartItemInfoDto> getCartItemInfoDto(CartItemDto cartItem) {
+    return cartService
+        .getCartInfo()
+        .map(
+            cartInfo ->
+                CartItemInfoDto.builder()
+                    .cartItem(cartItem)
+                    .cartItemsCount(cartInfo.getCartItemsCount())
+                    .cartAmount(cartInfo.getCartAmount())
+                    .build());
   }
 }
