7ea7708b343bb840292dbefe78a2b4b7a1851fb7
Pavel <strbnm@yandex.ru>
2025-03-08 16:50:07 +0300

    Рефакторинг класса OrderItemRepositoryTest для тестов R2DBC

--- src/test/java/ru/strbnm/store/repository/OrderItemRepositoryTest.java
+++ src/test/java/ru/strbnm/store/repository/OrderItemRepositoryTest.java
@@ -3,17 +3,18 @@ package ru.strbnm.store.repository;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import reactor.test.StepVerifier;
 import ru.strbnm.store.entity.Order;
 import ru.strbnm.store.entity.OrderItem;
 import ru.strbnm.store.entity.Product;
 
-public class OrderItemRepositoryTest extends BaseRepositoryTest {
+public class OrderItemRepositoryTest extends BaseR2dbcTest {
   @Autowired private OrderItemRepository orderItemRepository;
   @Autowired private ProductRepository productRepository;
   @Autowired private OrderRepository orderRepository;
@@ -42,54 +43,64 @@ public class OrderItemRepositoryTest extends BaseRepositoryTest {
     expectedOrderItems.put(5L, new OrderItemRecord(3L, 10, BigDecimal.valueOf(357.76), 3L));
     expectedOrderItems.put(6L, new OrderItemRecord(9L, 12, BigDecimal.valueOf(211.65), 3L));
 
-    List<OrderItem> orderItems = orderItemRepository.findAll();
-
-    HashMap<Long, OrderItemRecord> actualOrderItems = new HashMap<>();
-    for (OrderItem orderItem : orderItems) {
-      actualOrderItems.put(
-          orderItem.getId(),
-          new OrderItemRecord(
-              orderItem.getProduct().getId(),
-              orderItem.getQuantity(),
-              orderItem.getPrice(),
-              orderItem.getOrder().getId()));
-    }
-
-    assertThat("Должно быть 6 позиций товаров во всех заказах.", orderItems, hasSize(6));
-    assertEquals(
-        expectedOrderItems,
-        actualOrderItems,
-        "Ожидаемые и полученные позиции товаров в заказах должны быть равны.");
+    orderItemRepository
+        .findAll()
+        .collectList()
+        .as(StepVerifier::create)
+        .assertNext(
+            orderItems -> {
+              HashMap<Long, OrderItemRecord> actualOrderItems = new HashMap<>();
+              for (OrderItem orderItem : orderItems) {
+                actualOrderItems.put(
+                    orderItem.getId(),
+                    new OrderItemRecord(
+                        orderItem.getProductId(),
+                        orderItem.getQuantity(),
+                        orderItem.getPrice(),
+                        orderItem.getOrderId()));
+              }
+              assertThat("Должно быть 6 позиций товаров во всех заказах.", orderItems, hasSize(6));
+              assertEquals(
+                  expectedOrderItems,
+                  actualOrderItems,
+                  "Ожидаемые и полученные позиции товаров в заказах должны быть равны.");
+            })
+        .verifyComplete();
   }
 
   @Test
   void save_shouldAddOrderItemToDatabase() {
-    Product product =
-        productRepository.findById(5L).orElseThrow(() -> new RuntimeException("Товар не найден"));
-    Order order =
-        orderRepository.findById(1L).orElseThrow(() -> new RuntimeException("Заказ не найден"));
-    OrderItem orderItem =
-        OrderItem.builder()
-            .product(product)
-            .quantity(15)
-            .price(product.getPrice())
-            .order(order)
-            .build();
-
-    orderItemRepository.save(orderItem);
-
-    OrderItem savedOrderItem =
-        orderItemRepository.findAll().stream()
-            .filter(createdCartItem -> createdCartItem.getId().equals(7L))
-            .findFirst()
-            .orElse(null);
-    assertNotNull(savedOrderItem, "Полученный из БД объект не должен быть null.");
-    assertEquals(
-        "Description for product_5",
-        savedOrderItem.getProduct().getDescription(),
-        "Описание товара должно быть 'Description for product_5'.");
-    assertEquals(15, savedOrderItem.getQuantity(), "Количество товара быть равно 15.");
-    assertEquals(
-        1L, savedOrderItem.getOrder().getId(), "Идентификатор заказа должен быть равен 1.");
+    Long expectedProductId = 5L;
+    BigDecimal expectedProductPrice = BigDecimal.valueOf(432.91);
+    productRepository
+        .findById(expectedProductId)
+        .zipWith(orderRepository.findById(1L))
+        .flatMap(
+            tuple -> {
+              Product product = tuple.getT1();
+              Order order = tuple.getT2();
+              OrderItem orderItem =
+                  OrderItem.builder()
+                      .productId(product.getId())
+                      .quantity(15)
+                      .price(product.getPrice())
+                      .orderId(order.getId())
+                      .build();
+              return orderItemRepository.save(orderItem);
+            })
+        .flatMap(savedOrderItem -> orderItemRepository.findById(savedOrderItem.getId()))
+        .as(StepVerifier::create)
+        .assertNext(
+            savedOrderItem -> {
+              assertNotNull(savedOrderItem, "Полученный из БД объект не должен быть null.");
+              assertEquals(
+                  expectedProductPrice,
+                  savedOrderItem.getPrice(),
+                  "Цена товара должна быть '432.91'.");
+              assertEquals(15, savedOrderItem.getQuantity(), "Количество товара быть равно 15.");
+              assertEquals(
+                  1L, savedOrderItem.getOrderId(), "Идентификатор заказа должен быть равен 1.");
+            })
+        .verifyComplete();
   }
 }
