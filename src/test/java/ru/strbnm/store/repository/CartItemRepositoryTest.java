2a81dee92b6c42ad1180dc397b6fa65e62c4da00
Pavel <strbnm@yandex.ru>
2025-03-08 16:49:43 +0300

    Рефакторинг класса CartItemRepositoryTest для тестов R2DBC

--- src/test/java/ru/strbnm/store/repository/CartItemRepositoryTest.java
+++ src/test/java/ru/strbnm/store/repository/CartItemRepositoryTest.java
@@ -6,13 +6,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import reactor.test.StepVerifier;
 import ru.strbnm.store.entity.CartItem;
-import ru.strbnm.store.entity.Product;
 
-public class CartItemRepositoryTest extends BaseRepositoryTest {
+public class CartItemRepositoryTest extends BaseR2dbcTest {
     @Autowired private CartItemRepository cartItemRepository;
     @Autowired private ProductRepository productRepository;
 
@@ -36,68 +35,75 @@ public class CartItemRepositoryTest extends BaseRepositoryTest {
         expectedCartItems.put(3L, new CartItemRecord(6L, 30));
         expectedCartItems.put(4L, new CartItemRecord(9L, 1));
 
-        List<CartItem> cartItems = cartItemRepository.findAll();
-
-        HashMap<Long, CartItemRecord> actualCartItems = new HashMap<>();
-        for (CartItem cartItem : cartItems) {
-            actualCartItems.put(cartItem.getId(), new CartItemRecord(cartItem.getProduct().getId(), cartItem.getQuantity()));
-        }
-
-        assertThat("Должно быть 4 позиции в корзине.", cartItems, hasSize(4));
-        assertEquals(expectedCartItems, actualCartItems, "id, product_id и quantity должны совпадать.");
+        cartItemRepository.findAll()
+                .collectList()
+                .as(StepVerifier::create)
+                .assertNext(cartItems -> {
+                    HashMap<Long, CartItemRecord> actualCartItems = new HashMap<>();
+                    for (CartItem cartItem : cartItems) {
+                        actualCartItems.put(cartItem.getId(), new CartItemRecord(cartItem.getProductId(), cartItem.getQuantity()));
+                    }
+                    assertThat("Должно быть 4 позиции в корзине.", cartItems, hasSize(4));
+                    assertEquals(expectedCartItems, actualCartItems, "id, product_id и quantity должны совпадать.");
+                })
+                .verifyComplete();
     }
 
-  @Test
-  void findById_shouldReturnCartItemsWithSpecificId() {
-    CartItem cartItem =
-        cartItemRepository
-            .findById(2L)
-            .orElseThrow(() -> new RuntimeException("Позиция товара в корзине не найдена"));
 
-    assertEquals(4L, cartItem.getProduct().getId(), "id товара должно быть 4.");
-    assertEquals(20, cartItem.getQuantity(), "Количество товара должно быть 20.");
-  }
+    @Test
+    void findById_shouldReturnCartItemsWithSpecificId() {
+        cartItemRepository.findById(2L)
+                .as(StepVerifier::create)
+                .assertNext(cartItem -> {
+                    assertEquals(4L, cartItem.getProductId(), "id товара должно быть 4.");
+                    assertEquals(20, cartItem.getQuantity(), "Количество товара должно быть 20.");
+                })
+                .verifyComplete();
+    }
+
 
     @Test
     void save_shouldAddCartItemToDatabase() {
-        Product product = productRepository.findById(5L).orElseThrow(() -> new RuntimeException("Товар не найден"));
-        CartItem cartItem = CartItem.builder()
-                .product(product)
-                .quantity(15)
-                .build();
-
-        cartItemRepository.save(cartItem);
-
-        CartItem savedCartItem =
-                cartItemRepository.findAll().stream()
-                        .filter(createdCartItem -> createdCartItem.getId().equals(5L))
-                        .findFirst()
-                        .orElse(null);
-
-        assertNotNull(savedCartItem, "Полученный из БД объект не должен быть null.");
-        assertEquals(
-                "Description for product_5",
-                savedCartItem.getProduct().getDescription(),
-                "Описание товара должно быть 'Description for product_5'.");
-    assertEquals(15, savedCartItem.getQuantity(), "Количество товара быть равно 15.");
+        productRepository.findById(5L)
+                .flatMap(product -> {
+                    CartItem cartItem = CartItem.builder()
+                            .productId(product.getId())
+                            .quantity(15)
+                            .build();
+                    return cartItemRepository.save(cartItem);
+                })
+                .flatMap(savedCartItem -> cartItemRepository.findById(savedCartItem.getId()))
+                .as(StepVerifier::create)
+                .assertNext(savedCartItem -> {
+                    assertNotNull(savedCartItem, "Полученный из БД объект не должен быть null.");
+                    assertEquals(5L, savedCartItem.getProductId(), "ID товара должен быть 5.");
+                    assertEquals(15, savedCartItem.getQuantity(), "Количество товара должно быть равно 15.");
+                })
+                .verifyComplete();
     }
 
     @Test
     void deleteById_shouldRemoveCartItemWithSpecifiedIdFromDatabase() {
-        cartItemRepository.deleteById(3L);
-
-        List<CartItem> cartItems = cartItemRepository.findAll();
-        List<CartItem> deletedCartItem = cartItems.stream().filter(cartItem -> cartItem.getId().equals(3L)).toList();
-
-        assertTrue(deletedCartItem.isEmpty(), "Должен быть пустой список результатов для позиции корзины с id=3.");
+        cartItemRepository.deleteById(3L) // Удаляем элемент
+                .then(cartItemRepository.findAll().collectList()) // Получаем все элементы после удаления
+                .as(StepVerifier::create)
+                .assertNext(cartItems -> {
+                    boolean isDeleted = cartItems.stream().noneMatch(cartItem -> cartItem.getId().equals(3L));
+                    assertTrue(isDeleted, "Должен быть пустой список результатов для позиции корзины с id=3.");
+                })
+                .verifyComplete();
     }
 
     @Test
     void findByProduct_shouldReturnCartItemWhereProductEqualsOrNull() {
-        Product product = productRepository.findById(9L).orElseThrow(() -> new RuntimeException("Товар не найден"));
-        CartItem cartItem = cartItemRepository.findByProduct(product).orElse(null);
-
-        assertNotNull(cartItem, "Полученный из БД объект не должен быть null.");
-        assertEquals(product, cartItem.getProduct(), "Объекты типа Product должны быть равны.");
+      Long expectedProductId = 9L;
+        productRepository.findById(expectedProductId)
+                .flatMap(product -> cartItemRepository.findByProductId(product.getId()))
+                .as(StepVerifier::create)
+                .assertNext(cartItem -> {
+                    assertNotNull(cartItem, "Полученный из БД объект не должен быть null.");
+                    assertEquals(expectedProductId, cartItem.getProductId(), "Id товара должен быть равен 9.");
+                })
+                .verifyComplete();
     }
 }
