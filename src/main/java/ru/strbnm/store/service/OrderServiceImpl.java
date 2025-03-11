9c3caffaa432b5858afa998e03b9fad52f53d599
Pavel <strbnm@yandex.ru>
2025-03-11 15:32:08 +0300

    Лямбда выражения вынесены в приватные методы

--- src/main/java/ru/strbnm/store/service/OrderServiceImpl.java
+++ src/main/java/ru/strbnm/store/service/OrderServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import ru.strbnm.store.dto.CartItemWithProduct;
 import ru.strbnm.store.dto.OrderDto;
 import ru.strbnm.store.dto.OrderSummaryDto;
 import ru.strbnm.store.entity.Order;
@@ -19,90 +20,87 @@ import ru.strbnm.store.repository.OrderRepository;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-  private final CartItemRepository cartItemRepository;
-  private final OrderRepository orderRepository;
-  private final OrderItemRepository orderItemRepository;
-  private final OrderMapper orderMapper;
+    private final CartItemRepository cartItemRepository;
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final OrderMapper orderMapper;
 
-  @Autowired
-  public OrderServiceImpl(
-      CartItemRepository cartItemRepository,
-      OrderRepository orderRepository,
-      OrderItemRepository orderItemRepository,
-      OrderMapper orderMapper) {
-    this.cartItemRepository = cartItemRepository;
-    this.orderRepository = orderRepository;
-    this.orderItemRepository = orderItemRepository;
-    this.orderMapper = orderMapper;
-  }
+    @Autowired
+    public OrderServiceImpl(
+            CartItemRepository cartItemRepository,
+            OrderRepository orderRepository,
+            OrderItemRepository orderItemRepository,
+            OrderMapper orderMapper) {
+        this.cartItemRepository = cartItemRepository;
+        this.orderRepository = orderRepository;
+        this.orderItemRepository = orderItemRepository;
+        this.orderMapper = orderMapper;
+    }
 
-  @Override
-  @Transactional
-  public Mono<OrderDto> createOrder() {
-    return cartItemRepository
-        .findAllWithProducts()
-        .collectList()
-        .filter(cartItems -> !cartItems.isEmpty())
-        .switchIfEmpty(Mono.error(new RuntimeException("Корзина пустая.")))
-        .flatMap(
-            cartItems -> {
-              BigDecimal totalPrice =
-                  cartItems.stream()
-                      .map(
-                          item ->
-                              item.getProduct()
-                                  .getPrice()
-                                  .multiply(BigDecimal.valueOf(item.getCartItem().getQuantity())))
-                      .reduce(BigDecimal.ZERO, BigDecimal::add);
+    @Override
+    @Transactional
+    public Mono<OrderDto> createOrder() {
+        return cartItemRepository.findAllWithProducts()
+                .collectList()
+                .filter(cartItems -> !cartItems.isEmpty())
+                .switchIfEmpty(Mono.error(new RuntimeException("Корзина пустая.")))
+                .flatMap(this::processCartItems)
+                .map(orderMapper::toDTO);
+    }
 
-              Order newOrder = Order.builder().totalPrice(totalPrice).build();
+    private Mono<Order> processCartItems(List<CartItemWithProduct> cartItems) {
+        BigDecimal totalPrice = calculateTotalPrice(cartItems);
+        Order newOrder = Order.builder().totalPrice(totalPrice).build();
 
-              return orderRepository
-                  .save(newOrder)
-                  .flatMap(
-                      savedOrder -> {
-                        List<OrderItem> orderItems =
-                            cartItems.stream()
-                                .map(
-                                    cartItemWithProduct ->
-                                        OrderItem.builder()
-                                            .productId(cartItemWithProduct.getProduct().getId())
-                                            .quantity(
-                                                cartItemWithProduct.getCartItem().getQuantity())
-                                            .price(cartItemWithProduct.getProduct().getPrice())
-                                            .orderId(savedOrder.getId())
-                                            .build())
-                                .toList();
+        return orderRepository.save(newOrder)
+                .flatMap(savedOrder -> saveOrderItemsAndClearCart(cartItems, savedOrder));
+    }
 
-                        return orderItemRepository
-                            .saveAll(orderItems)
-                            .then(cartItemRepository.deleteAll())
-                            .thenReturn(savedOrder);
-                      });
-            })
-        .map(orderMapper::toDTO);
-  }
+    private BigDecimal calculateTotalPrice(List<CartItemWithProduct> cartItems) {
+        return cartItems.stream()
+                .map(item -> item.getProduct().getPrice()
+                        .multiply(BigDecimal.valueOf(item.getCartItem().getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 
-  @Override
-  public Flux<OrderDto> getAllOrders() {
-    return orderRepository.findAllOrdersWithItemsAndProducts();
-  }
+    private Mono<Order> saveOrderItemsAndClearCart(List<CartItemWithProduct> cartItems, Order savedOrder) {
+        List<OrderItem> orderItems = mapCartItemsToOrderItems(cartItems, savedOrder.getId());
 
-  @Override
-  public Mono<OrderDto> getOrderById(Long orderId) {
-    return orderRepository.findOrderWithItemsAndProducts(orderId);
-  }
+        return orderItemRepository.saveAll(orderItems)
+                .then(cartItemRepository.deleteAll())
+                .thenReturn(savedOrder);
+    }
 
-  @Override
-  public Mono<OrderSummaryDto> getOrdersSummary() {
-    return orderRepository
-        .findAllOrdersWithItemsAndProducts()
-        .reduce(
-            new OrderSummaryDto(new ArrayList<>(), BigDecimal.ZERO),
-            (summary, order) -> {
-              summary.getOrderDtoList().add(order);
-              return new OrderSummaryDto(
-                  summary.getOrderDtoList(), summary.getTotalAmount().add(order.getTotalPrice()));
-            });
-  }
+    private List<OrderItem> mapCartItemsToOrderItems(List<CartItemWithProduct> cartItems, Long orderId) {
+        return cartItems.stream()
+                .map(cartItemWithProduct ->
+                        OrderItem.builder()
+                                .productId(cartItemWithProduct.getProduct().getId())
+                                .quantity(cartItemWithProduct.getCartItem().getQuantity())
+                                .price(cartItemWithProduct.getProduct().getPrice())
+                                .orderId(orderId)
+                                .build())
+                .toList();
+    }
+
+    @Override
+    public Flux<OrderDto> getAllOrders() {
+        return orderRepository.findAllOrdersWithItemsAndProducts();
+    }
+
+    @Override
+    public Mono<OrderDto> getOrderById(Long orderId) {
+        return orderRepository.findOrderWithItemsAndProducts(orderId);
+    }
+
+    @Override
+    public Mono<OrderSummaryDto> getOrdersSummary() {
+        return orderRepository.findAllOrdersWithItemsAndProducts()
+                .reduce(new OrderSummaryDto(new ArrayList<>(), BigDecimal.ZERO), this::accumulateOrderSummary);
+    }
+
+    private OrderSummaryDto accumulateOrderSummary(OrderSummaryDto summary, OrderDto order) {
+        summary.getOrderDtoList().add(order);
+        return new OrderSummaryDto(summary.getOrderDtoList(), summary.getTotalAmount().add(order.getTotalPrice()));
+    }
 }
